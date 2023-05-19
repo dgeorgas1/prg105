f74a40a59030c3149a76715c2d40b3b3b201060e
Daniel Georgas <133694944+dgeorgas1@users.noreply.github.com>
2023-05-19 17:59:40 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,43 @@
+"""Chapter 02 Practice 1 - Mad-libs
+
+Assignment Requirements:
+Turn in your modified file that accepts input from the user,
+assigns the input to a minimum of 8 variables,
+and uses the variables in the output which essentially creates a mad-libs game.
+"""
+
+first_line = input("Enter a noun: ")
+second_line = input("Enter an adjective: ")
+third_line = input("Enter a noun: ")
+forth_line = input("Enter a noun: ")
+fifth_line = input("Enter an adjective: ")
+sixth_line = input("Enter an adverb: ")
+seventh_line = input("Enter a pronoun: ")
+eighth_line = input("Enter a noun: ")
+ninth_line = input("Enter a noun: ")
+tenth_line = input("Enter an adjective: ")
+eleventh_line = input("Enter a pronoun: ")
+twelfth_line = input("Enter an adjective: ")
+thirteenth_line = input("Enter a noun: ")
+fourteenth_line = input("Enter a noun: ")
+fifteenth_line = input("Enter a pronoun: ")
+sixteenth_line = input("Enter an adjective: ")
+
+print("\nI can still remember me and " + first_line)
+print(second_line + " and strange")
+print("Always so inclined, coloring outside the " + third_line)
+print("Yeah, you were never on " + forth_line + "\n")
+
+print("You've always been slightly " + fifth_line + ", kind of weird")
+print("Upside " + sixth_line + " and not all here")
+print("What's wrong with me and " + seventh_line + " is crystal clear\n")
+
+print("Sometimes I'm in a " + eighth_line + " where I don't belong")
+print("And the " + ninth_line + " is on fire and there's no alarm")
+print("And the walls are " + tenth_line + " too")
+print("How 'bout " + eleventh_line + "?")
+print("I've never been the " + twelfth_line + ", thought I'd seen it all")
+print("'Til I got my " + thirteenth_line + " to the lunatic ball")
+print("And my " + fourteenth_line + " are coming' too")
+print("How 'bout " + fifteenth_line + "?")
+print("Don't worry, it's all just a symptom of being " + sixteenth_line)
