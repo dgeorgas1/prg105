e3011d5755629a599afa3c8fb8b047ab653212c1
Daniel Georgas <133694944+dgeorgas1@users.noreply.github.com>
2023-07-13 20:56:39 -0500

    Add files via upload

---
+++ office_furniture_class.py
@@ -0,0 +1,52 @@
+class OfficeFurniture:
+    # Initializer
+    def __init__(self, category, material, length, width, height, price):
+        self.__category = category
+        self.__material = material
+        self.__length = length
+        self.__width = width
+        self.__height = height
+        self.__price = price
+
+    # str Method
+    def __str__(self):
+        return f'Category: {self.__category} \nMaterial: {self.__material} \nLength: {self.__length}' \
+               f'\nWidth: {self.__width} \nHeight: {self.__height} \nPrice: {self.__price}'
+
+    # Mutators
+    def set_category(self, category):
+        self.__category = category
+
+    def set_material(self, material):
+        self.__material = material
+
+    def set_length(self, length):
+        self.__length = length
+
+    def set_width(self, width):
+        self.__width = width
+
+    def set_height(self, height):
+        self.__height = height
+
+    def set_price(self, price):
+        self.__price = price
+
+    # Accessors
+    def get_category(self):
+        return self.__category
+
+    def get_material(self):
+        return self.__material
+
+    def get_length(self):
+        return self.__length
+
+    def get_width(self):
+        return self.__width
+
+    def get_height(self):
+        return self.__height
+
+    def get_price(self):
+        return self.__price
